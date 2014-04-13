1de9af2e844d83c67ef1e747d2653d4220d403f2
Wang Chaohui <CH.WANG@ntu.edu.sg>
2014-04-13 16:02:53 +0800

    BigNum::MulBig

--- big_num.cpp
+++ big_num.cpp
@@ -8,45 +8,74 @@ typedef long long ll;
 class BigNum
 {
 public:
-    BigNum();
+    BigNum() : size_(1) { d_[0] = 0; }
     BigNum(int x);
+    BigNum(const char *s);
     void To(int x);
-    void Print();
+    void Print() const;
     void MulInt(int x);
+    BigNum MulBig(const BigNum &y) const;
 
 private:
+    static const int kSize = 60;
     static const int kDigits = 4;
     static const int kBase = 10000;
-    int size;
-    int d[60];
+    static void Check(bool b) { while (!b)puts("error"); }
+    int size_;
+    int d_[kSize];
 };
 
-BigNum::BigNum() : size(1)
+BigNum::BigNum(int x) : size_(0)
 {
-    d[0] = 0;
+    for (; !size_ || x; d_[size_++] = x % kBase, x /= kBase);
 }
 
-BigNum::BigNum(int x) : size(0)
+BigNum::BigNum(const char *s)
 {
-    for (; !size || x; d[size++] = x % kBase, x /= kBase);
+    while (*s == '0')s++;
+    if (!*s){ To(1); return; }
+    int l = strlen(s);
+    size_ = (l - 1) / kDigits + 1;
+    int p = size_ - 1; d_[p] = 0;
+    for (int i = 0; i < l; i++)
+    {
+        if (i == l - p * kDigits)d_[--p] = 0;
+        d_[p] = d_[p] * 10 + s[i] - '0';
+    }
 }
 
 void BigNum::To(int x)
 {
-    size = 1; d[0] = x;
+    size_ = 1; d_[0] = x;
 }
 
-void BigNum::Print()
+void BigNum::Print() const
 {
-    printf("%d", d[size - 1]);
-    for (int i = size - 2; i >= 0; i--)
-        printf("%0*d", kDigits, d[i]);
+    printf("%d", d_[size_ - 1]);
+    for (int i = size_ - 2; i >= 0; i--)
+        printf("%0*d", kDigits, d_[i]);
 }
 
 void BigNum::MulInt(int x)
 {
     if (x == 0) { To(0); return; }
     int c = 0;
-    for (int i = 0; i < size; i++){ ll t = (ll)d[i] * x + c; c = static_cast<int>(t / kBase); d[i] = t % kBase; }
-    for (; c; c /= kBase) d[size++] = c % kBase;
+    for (int i = 0; i < size_; i++){ ll t = (ll)d_[i] * x + c; c = static_cast<int>(t / kBase); d_[i] = t % kBase; }
+    for (; c; c /= kBase) d_[size_++] = c % kBase;
+}
+
+BigNum BigNum::MulBig(const BigNum &y) const
+{
+    int l = size_ + y.size_;
+    Check(l <= kSize);
+    BigNum z;
+    memset(z.d_, 0, sizeof(z.d_));
+    for (int a = 0; a < size_; a++)for (int b = 0; b < y.size_; b++)
+    {
+        z.d_[a + b] += d_[a] * y.d_[b];
+        if (z.d_[a + b] >= kBase) { z.d_[a + b + 1] += z.d_[a + b] / kBase; z.d_[a + b] %= kBase; }
+        /* carefully */
+    }
+    for (z.size_ = l; z.size_ > 1 && !z.d_[z.size_ - 1]; z.size_--);
+    return z;
 }
