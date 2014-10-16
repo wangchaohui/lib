609da58b4a0c6446046025c055e35bc502b03722
Chaohui Wang <wangchaohui@hotmail.com>
2014-10-16 16:49:52 +0800

    egcd 2

--- gcd.cpp
+++ gcd.cpp
@@ -5,7 +5,7 @@ int gcd(int a, int b) {
 int egcd(int a, int b, int &x, int &y) {
   if (b == 0) { x = 1; y = 0; return a; }
   int d = egcd(b, a % b, y, x);
-  y = y - (a / b) * x;
+  y -= a / b * x;
   return d;
 }
 
