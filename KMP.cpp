c2eebc6c2246f177af01cbcccfd38a0a35d7cfad
Chaohui Wang <wangchaohui@hotmail.com>
2014-11-04 18:58:36 +0800

    KMP

--- KMP.cpp
+++ KMP.cpp
@@ -1,36 +1,31 @@
-class KMP
-{
-    int t_[1000005];
-    int word_lenth_;
-    const char* word_;
+class KMP {
+  int t_[1005];
+  int n_;
+  const char* word_;
 
 public:
-    KMP(char *word);
-    void match(char *s);
+  KMP(const char *word);
+  void match(const char *s) const;
 };
 
-KMP::KMP(char *word) : word_(word), word_lenth_(strlen(word))
-{
-    t_[0] = t_[1] = 0;
-    int j = 0;
-    for (int i = 2; i <= word_lenth_; i++)
-    {
-        while (j > 0 && word[j] != t_[i - 1]) j = t_[j];
-        if (word[j] == word[i - 1]) j++;
-        t_[i] = j;
-    }
+KMP::KMP(const char *word) : word_(word), n_(strlen(word)) {
+  t_[0] = t_[1] = 0;
+  int j = 0;
+  for (int i = 1; i < n_; i++) {
+    while (j > 0 && word[j] != word[i]) j = t_[j];
+    if (word[j] == word[i]) j++;
+    t_[i + 1] = j;
+  }
+  for (int i = 0; i <= n_; i++) printf("%d ", t_[i]);
 }
 
-void KMP::match(char *s)
-{
-    int j = 0;
-    for (int i = 0; s[i]; i++)
-    {
-        while (j > 0 && word_[j] != s[i]) j = t_[j];
-        if (word_[j] == s[i]) j++;
-        if (j == word_lenth_)
-        {
-            j = t_[j];
-        }
+void KMP::match(const char *s) const {
+  int j = 0;
+  for (int i = 0; s[i]; i++) {
+    while (j > 0 && word_[j] != s[i]) j = t_[j];
+    if (word_[j] == s[i]) j++;
+    if (j == n_) {
+      j = t_[j];
     }
+  }
 }
